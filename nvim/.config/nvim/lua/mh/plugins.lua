97f7a6f394366da336d36e2cf9142e12a533eee2
Martin Hoejmann <martin@mail.hoejmann.org>
2021-03-07 19:27:11 +0100

    Added telescope

--- nvim/.config/nvim/lua/mh/plugins.lua
+++ nvim/.config/nvim/lua/mh/plugins.lua
@@ -2,5 +2,11 @@ return require('packer').startup(function()
   use { 'wbthomason/packer.nvim' }
   use { 'neovim/nvim-lspconfig' }
   use { 'hrsh7th/nvim-compe' }
+  use { 'nvim-telescope/telescope.nvim',
+    requires = {
+      {'nvim-lua/popup.nvim'},
+      {'nvim-lua/plenary.nvim'}
+    }
+  }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 end)
